c0a30b38984015bae3aa9ddb11c2267e1803215e
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2020-12-27 22:43:24 +0900

    Fix bug of sessions_helper

--- back/app/helpers/sessions_helper.rb
+++ back/app/helpers/sessions_helper.rb
@@ -6,9 +6,9 @@ module SessionsHelper
 
    # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    if session[:user_id]
+    if logged_in?
      #@current_user = @current_user || User.find_by(id: session[:user_id])と同じ意味
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= TUser.find_by(id: session[:user_id])
     end
   end
 
@@ -19,7 +19,7 @@ module SessionsHelper
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
-    p session[:user_id]
+    session[:user_id]
   end
 
   # 現在のユーザーをログアウトする
