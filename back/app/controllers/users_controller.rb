c802376f6eda11b8beff078f32ef63ec58297548
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2020-12-28 22:02:17 +0900

    Refactor

--- back/app/controllers/users_controller.rb
+++ back/app/controllers/users_controller.rb
@@ -5,45 +5,45 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @t_users = TUser.all
+    @users = TUser.all
 
-    render json: @t_users
+    render json: @users
   end
 
   # GET /users/1
   def show
-    render json: @t_user
+    render json: @user
   end
 
   # POST /users
   def create
-    @t_user = TUser.new(user_params)
+    @user = TUser.new(user_params)
 
-    if @t_user.save
-      render json: @t_user, status: :created
+    if @user.save
+      render json: @user, status: :created
     else
-      render json: @t_user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @t_user.update(user_params)
-      render json: @t_user
+    if @user.update(user_params)
+      render json: @user
     else
-      render json: @t_user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :internal_server_error
     end
   end
 
   # DELETE /users/1
   def destroy
-    @t_user.destroy
+    @user.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @t_user = TUser.find(params[:id])
+      @user = TUser.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
