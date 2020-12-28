7cd7ae235d02db63d3fecde1256102a5cc7296e6
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2020-12-29 00:13:33 +0900

    Refactor

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
 
   # session
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/login',             to: 'sessions#new'
+  post   '/login',             to: 'sessions#create'
+  delete '/logout',            to: 'sessions#destroy'
 
   # workflow
-  get    '/workflow', to: 'workflows#getWork'
+  get    '/workflow',          to: 'workflows#getWork'
   put    '/workflow/complete', to: 'workflows#complete'
-  put    '/workflow/undo', to: 'workflows#undo'
+  put    '/workflow/undo',     to: 'workflows#undo'
   
   # user
   resources :users
 
   # experiment
   resources :experiments
+
+  # assessment
+  get    '/assessments',       to: 'assessments#getAssessmentWork'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
