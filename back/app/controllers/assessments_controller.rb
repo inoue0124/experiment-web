39d932b92ebe1905fbb5087fcda6cf1f2bdc5a69
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2020-12-30 01:23:19 +0900

    assessment update apiの高速化

--- back/app/controllers/assessments_controller.rb
+++ back/app/controllers/assessments_controller.rb
@@ -27,17 +27,26 @@ class AssessmentsController < ApplicationController
     @t_assessment = TAssessment.find_by(t_workflow_id: params[:workflow_id])
     @d_assessments = DAssessment.where(t_assessment_id: @t_assessment.id)
 
+    # バルクインサート用配列
+    @new_d_assess = []
     for file_num in 1..@t_assessment.num_files do
       @d = params[:_json].find {|data| data[:file_number] == file_num}
 
       if @d_assessments.exists?(file_number: file_num)
         @d_assessment = @d_assessments.find_by(file_number: file_num)
-        @d_assessment.update(@d.permit(:t_assessment_id, :t_user_id, :file_number, :score, :comment).to_h)
+
+        # 変更があるデータのみupdate
+        if @d[:score] != @d_assessment[:score] || @d[:comment] != @d_assessment[:comment]
+          @d_assessment.update(@d.permit(:t_assessment_id, :t_user_id, :file_number, :score, :comment).to_h)
+        end
+
       else
-        @d_assessment = DAssessment.new(@d.permit(:t_assessment_id, :t_user_id, :file_number, :score, :comment).to_h)
-        @d_assessment.save
+        @new_d_assess << DAssessment.new(@d.permit(:t_assessment_id, :t_user_id, :file_number, :score, :comment).to_h)
       end
     end
+
+    # バルクインサート実行
+    DAssessment.import @new_d_assess
   end
 
 end
