1edfe84bd805ee3c925f7b3e9e01fb20f2addc08
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2020-12-30 13:39:30 +0900

    Add crud function base to user management func

--- front/pages/admin/users/index.vue
+++ front/pages/admin/users/index.vue
@@ -1,44 +1,200 @@
 <template>
-  <section>
-    <div>
-      <h1>New user</h1>
-      <form @submit.prevent="post">
+  <v-data-table
+    :headers="headers"
+    :items="users"
+    sort-by="id"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>ユーザ管理</v-toolbar-title>
+        <v-spacer></v-spacer>
 
-        <label for="email">Email: </label>
-        <input id="email" v-model="email" type="text" name="email" />
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              新規登録
+            </v-btn>
+          </template>
 
-        <label for="password">Password: </label>
-        <input id="password" v-model="password" type="password" name="password" />
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
 
-        <label for="password_confirmation">Password Confirmation: </label>
-        <input id="password_confirmation" v-model="password_confirmation" type="password" name="password_confirmation" />
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.uuid"
+                      label="ユニークID"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.email"
+                      label="Email"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.t_experiment_id"
+                      label="実験ID"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
 
-        <button type="submit">submit</button>
-      </form>
-    </div>
-  </section>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">キャンセル</v-btn>
+              <v-btn color="blue darken-1" text @click="save">保存</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">削除してよろしいですか?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">キャンセル</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">削除</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+
+    <template v-slot:item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+    </template>
+
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">再読み込み</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 import UserApi from '@/plugins/axios/modules/user'
 
-export default {
-  layout: 'admin',
-  middleware: 'admin-redirector',
+  export default {
+    layout: 'admin',
+    middleware: 'admin-redirector',
+    
+    data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: 'id',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'uuid', value: 'uuid' },
+        { text: 'メールアドレス', value: 'email' },
+        { text: '実験ID', value: 't_experiment_id' },
+        { text: '終了したワークフローID', value: 'done_workflow_id' },
+        { text: '終了したワークフロー名', value: 'done_workflow_name' },
+        { text: '操作', value: 'actions', sortable: false },
+      ],
+      users: [],
+      editedIndex: -1,
+      editedItem: {
+        uuid: '',
+        email: '',
+        t_experiment_id: 0
+      },
+      defaultItem: {
+        uuid: '',
+        email: '',
+        t_experiment_id: ''
+      },
+    }),
 
-  data() {
-    return {
-      email: '',
-      password: '',
-      password_confirmation: ''
-    }
-  },
-  methods: {
-    post() {
-      UserApi.createUser(this.email, this.password, this.password_confirmation).then((res) => {
-        this.$router.push(`users/${res.id}`)
-      })
-    }
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? '新規登録' : '編集'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        UserApi.getUsers().then((res) => {
+          this.users = res
+          console.log(this.users)
+        })
+      },
+
+      editItem (item) {
+        this.editedIndex = this.users.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.users.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.users.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.users[this.editedIndex], this.editedItem)
+        } else {
+          this.users.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
   }
-}
 </script>
+
