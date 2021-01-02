c1e2f18c6ef22bb192df3701c8a540d01d65a455
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-01-02 19:28:40 +0900

    Refactor

--- front/pages/admin/experiments/_id.vue
+++ front/pages/admin/experiments/_id.vue
@@ -1,58 +1,58 @@
 <template>
-    <v-row>
-      <v-col cols="12" sm="12" md="12">
-        <v-toolbar flat class="mb-3">
-          <a link @click="goBack">実験一覧</a>/編集
-          <v-spacer></v-spacer>
-          <v-btn color="error" @click="openDeleteDialog" class="mr-5">この実験を削除</v-btn>
-          <v-btn color="primary" @click="confirmRegister">編集内容を保存</v-btn>
-          <ConfirmDialog
-            ref="confirm"
-            title="削除確認"
-            :message="deleteMessage"
-            buttonMessage="削除"
-            @confirm="confirmDelete"
-            color="error"
-          >
-          </ConfirmDialog>
-        </v-toolbar>
+  <v-row>
+    <v-col cols="12" sm="12" md="12">
+      <v-toolbar flat class="mb-3">
+        <a link @click="goBack">実験一覧</a>/編集
+        <v-spacer></v-spacer>
+        <v-btn color="error" @click="openDeleteDialog" class="mr-5">この実験を削除</v-btn>
+        <v-btn color="primary" @click="confirmRegister">編集内容を保存</v-btn>
+        <ConfirmDialog
+          ref="confirm"
+          title="削除確認"
+          :message="deleteMessage"
+          buttonMessage="削除"
+          @confirm="confirmDelete"
+          color="error"
+        >
+        </ConfirmDialog>
+      </v-toolbar>
 
-        <v-card-text>
-          <p class="mb-3 card-title">実験名設定</p>
-          <v-card class=" pa-5 mb-16">
-            <v-card-text>
-              <v-text-field
-                label="実験名"
-                v-model="name"
-                required
-              ></v-text-field>
-            </v-card-text>
-          </v-card>
+      <v-card-text>
+        <p class="mb-3 card-title">実験名設定</p>
+        <v-card class=" pa-5 mb-16">
+          <v-card-text>
+            <v-text-field
+              label="実験名"
+              v-model="name"
+              required
+            ></v-text-field>
+          </v-card-text>
+        </v-card>
 
-          <div v-for="(work, key) in workflow" :key="key">
-            <div v-if="work=='agreement'">
-              <p class="mb-3 card-title">同意書設定</p>
-              <RegisterAgreementCard class=" pa-5 mb-16" ref="agreement" :claim_text_prop="work_data[key].text"></RegisterAgreementCard>
-            </div>
-            
-            <div v-if="work=='facesheet'">
-              <p class="mb-3 card-title">フェイスシート設定</p>
-              <RegisterFacesheetCard class=" pa-5 mb-16" ref="facesheet"></RegisterFacesheetCard>
-            </div>
-
-            <div v-if="work=='assessment'">
-              <p class="mb-3 card-title">評価実験設定</p>
-              <UpdateAssessmentCard class=" pa-5 mb-16" ref="assessment" :assessment_prop="work_data[key]"></UpdateAssessmentCard>
-            </div>
-            
-            <div v-if="work=='questionnaire'">
-              <p class="mb-3 card-title">アンケート設定</p>
-              <RegisterQuestionnaireCard class=" pa-5 mb-16" ref="questionnaire" :form_url_prop="work_data[key].url"></RegisterQuestionnaireCard>
-            </div>
+        <div v-for="(work, key) in workflow" :key="key">
+          <div v-if="work=='agreement'">
+            <p class="mb-3 card-title">同意書設定</p>
+            <RegisterAgreementCard class=" pa-5 mb-16" ref="agreement" :claim_text_prop="work_data[key].text"></RegisterAgreementCard>
           </div>
-        </v-card-text>
-      </v-col>
-    </v-row>
+          
+          <div v-if="work=='facesheet'">
+            <p class="mb-3 card-title">フェイスシート設定</p>
+            <RegisterFacesheetCard class=" pa-5 mb-16" ref="facesheet"></RegisterFacesheetCard>
+          </div>
+
+          <div v-if="work=='assessment'">
+            <p class="mb-3 card-title">評価実験設定</p>
+            <UpdateAssessmentCard class=" pa-5 mb-16" ref="assessment" :assessment_prop="work_data[key]"></UpdateAssessmentCard>
+          </div>
+          
+          <div v-if="work=='questionnaire'">
+            <p class="mb-3 card-title">アンケート設定</p>
+            <RegisterQuestionnaireCard class=" pa-5 mb-16" ref="questionnaire" :form_url_prop="work_data[key].url"></RegisterQuestionnaireCard>
+          </div>
+        </div>
+      </v-card-text>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
