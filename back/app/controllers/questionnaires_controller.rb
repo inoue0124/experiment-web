7fdd992f96729d44a3aafae1a7edc88e136b601f
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-01-05 21:06:55 +0900

    googleフォームのURLを変換する

--- back/app/controllers/questionnaires_controller.rb
+++ back/app/controllers/questionnaires_controller.rb
@@ -3,6 +3,7 @@ class QuestionnairesController < ApplicationController
   # GET /questionnaires/:workflow_id
   def show
     @t_questionnaire = TQuestionnaire.find_by(t_workflow_id: params[:workflow_id])
+    @t_questionnaire[:url] = @t_questionnaire[:url].sub('viewform?usp=sf_link', 'viewform?embedded=true')
     render json: @t_questionnaire, status: :ok
   end
 end
