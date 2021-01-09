b8ef52acc972db1311c099acd969bb5b32b1f9e2
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-01-09 21:59:32 +0900

    同意書の体裁を整えた

--- front/pages/agreement/_id.vue
+++ front/pages/agreement/_id.vue
@@ -17,6 +17,18 @@
         ></v-checkbox>
 
         <v-btn color="primary" type="submit" @click="next">次へ進む</v-btn>
+
+        <v-card class="mt-16">
+          <v-card-text>
+            <p>
+              ボイクマン総子(Ph.D.)<br>
+              東京大学 大学院総合文化研究科<br>
+              グローバルコミュニケーション研究センター、教授<br>
+              153-8902　東京都目黒区駒場3-8-1 東京大学<br>
+              beuckmann@g.ecc.u-tokyo.ac.jp
+            </p>
+          </v-card-text>
+        </v-card>
       </div>
 
     </v-col>
@@ -49,7 +61,7 @@ export default {
 
   mounted() {
     AgreementApi.getAgreement(this.$route.params.id).then((res) => {
-      this.claim_text = res.text
+      this.claim_text = res.text.replace(/<p><br><\/p>/g, '')
     })
   },
 
