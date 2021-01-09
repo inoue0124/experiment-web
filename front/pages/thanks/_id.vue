b0ee95416ecb329a8b4730deda310c6eec32e034
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-01-09 19:37:47 +0900

    thanks画面の文言修正

--- front/pages/thanks/_id.vue
+++ front/pages/thanks/_id.vue
@@ -1,15 +1,23 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="6" md="6">
       <v-card class="pa-md-10 mt-16">
         <v-card-title class="headline">
           実験にご参加いただき誠にありがとうございました。
         </v-card-title>
 
         <v-card-text>
-          お問い合わせは
-          <a href="mailto:example@mail.com" target="_blank">example@mail.com</a>
-          までお願いします。
+          <p>
+            本実験の内容に関するお問い合わせは、下記の連絡先へお願いします。<br>
+          </p>
+
+          <p>
+            ボイクマン総子(Ph.D.)<br>
+            東京大学 大学院総合文化研究科<br>
+            グローバルコミュニケーション研究センター、教授<br>
+            153-8902　東京都目黒区駒場3-8-1 東京大学<br>
+            beuckmann@g.ecc.u-tokyo.ac.jp
+          </p>
         </v-card-text>
 
       </v-card>
