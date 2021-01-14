ce4072258eb4e8cb4d1dbab01c4349d93b60aa84
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-01-15 00:18:47 +0900

    facesheetをgoogle formに変更

--- front/components/modals/CreateExperimentModal.vue
+++ front/components/modals/CreateExperimentModal.vue
@@ -96,7 +96,8 @@ export default {
       dialog: false,
       workflow: [
         "agreement",
-        "facesheet",
+        //"facesheet",
+        "questionnaire",
         "assessment",
         "questionnaire",
         "transfer",
