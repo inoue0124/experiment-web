fb0a8c84a81a97801dcb8e701f54f9638e12d4c0
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-01-15 00:14:33 +0900

    form urlを変更

--- back/app/controllers/questionnaires_controller.rb
+++ back/app/controllers/questionnaires_controller.rb
@@ -2,8 +2,11 @@ class QuestionnairesController < ApplicationController
 
   # GET /questionnaires/:workflow_id
   def show
+    @t_user = current_user
     @t_questionnaire = TQuestionnaire.find_by(t_workflow_id: params[:workflow_id])
     @t_questionnaire[:url] = @t_questionnaire[:url].sub('viewform?usp=sf_link', 'viewform?embedded=true')
+    @t_questionnaire[:url] = @t_questionnaire[:url].sub('viewform?usp=pp_url', 'viewform?embedded=true')
+    @t_questionnaire[:url] = @t_questionnaire[:url].sub('uuid', @t_user.uuid)
     render json: @t_questionnaire, status: :ok
   end
 end
