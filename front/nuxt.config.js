ca2d2a05c8cfdd8f6b6ba198289f6ce50bfc026a
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-02-08 00:12:29 +0900

    target: serverに変更

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   ssr: false,
-  target: 'static',
+  target: 'server',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - experiment-web',
